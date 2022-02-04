e84da5ab88d68b1845eaa4ff45b0fe5f12a70df4
Zahid097 <98472087+Zahid097@users.noreply.github.com>
2022-02-04 12:35:08 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Uchburchtuk talaa = new Uchburchtuk();
+        talaa.a = 10;
+        talaa.b = 10;
+        talaa.c = 5;
+        talaa.area(talaa.a, talaa.b, talaa.c);
+
+    }
+}
